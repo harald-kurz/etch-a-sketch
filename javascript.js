fa5deb3497c985b60b6049249578e9a61fc2cb74
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-02 12:34:25 +0100

    16x16 grid with flexbox

---
+++ javascript.js
@@ -0,0 +1,19 @@
+let gridSize = 16;
+
+const container = document.querySelector('#container');
+
+function createGrid(){
+  for (let index = 0; index < (gridSize*gridSize); index++) {
+    const div = document.createElement('div');
+    const divWidth = (100 / gridSize) - 0.4;
+
+    div.style.border = '1px solid black';
+    div.style.flexGrow = 1;
+    div.style.flexShrink = 0;
+    div.style.flexBasis = `${divWidth}%`;
+
+    container.appendChild(div);
+  }
+}
+
+createGrid();
