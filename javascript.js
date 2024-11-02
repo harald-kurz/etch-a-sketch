2b9c2ef600b3f4580e6626b71892702fe839d8bf
Harald Kurz <46395850+harald-kurz@users.noreply.github.com>
2024-11-02 12:56:16 +0100

    fix border problem on grid items

--- javascript.js
+++ javascript.js
@@ -17,12 +17,12 @@ function resetGrid() {
 function createGrid(){
   for (let index = 0; index < (gridSize*gridSize); index++) {
     const div = document.createElement('div');
-    const divWidth = (100 / gridSize) - 0.4;
+    const divWidth = (600 / gridSize) - 1.6;
 
     div.style.border = '1px solid black';
     div.style.flexGrow = 1;
     div.style.flexShrink = 0;
-    div.style.flexBasis = `${divWidth}%`;
+    div.style.flexBasis = `${divWidth}px`;
 
     div.addEventListener('mouseenter', function() {
       this.style.backgroundColor = 'black';
